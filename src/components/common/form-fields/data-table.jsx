7d50fd045a1a8d3ab85426b40e1d786f813863bb
Mehmet Genç <gencaslan555@gmail.com>
2024-05-10 23:28:07 +0300

    data-table latest version

--- src/components/common/form-fields/data-table.jsx
+++ src/components/common/form-fields/data-table.jsx
@@ -1,19 +1,30 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-
 export const Column = ({ children }) => {
   return <th scope="col">{children}</th>;
 };
 
-export const Row = ({ children, selectionMode }) => {
-  return <tr>{children}</tr>;
+//!DOM da tanımlı eventler onCLick onChange alabilir
+//!Row componenti DOM da olmayan bir element
+//!o yüzden onClick i row un propu olarak alıp componentte karşılayacağız
+export const Row = ({ children, selectionMode, onClick, selected }) => {
+  const isSelectable = selectionMode !== "none";
+
+  return (
+    <tr
+      onClick={onClick}
+      className={isSelectable && selected ? "table-primary" : ""}
+      style={{ cursor: isSelectable ? "pointer" : "auto" }}
+    >
+      {children}
+    </tr>
+  );
 };
 
 export const Cell = ({ children }) => {
   return <td>{children}</td>;
 };
-
 export const FirstPageButton = ({ currentPage }) => {
   return (
     <li className="page-item">
@@ -26,7 +37,6 @@ export const FirstPageButton = ({ currentPage }) => {
     </li>
   );
 };
-
 export const PreviousPageButton = ({ currentPage }) => {
   return (
     <li className="page-item">
@@ -39,15 +49,23 @@ export const PreviousPageButton = ({ currentPage }) => {
     </li>
   );
 };
-
 export const PageButton = ({ totalPages, currentPage }) => {
-  return [...new Array(totalPages)].map((_, index) => (
+  const totalAmountOfButtons = 5;
+  const averageOfTotalButtons = Math.floor(totalAmountOfButtons / 2);
+  let startPage = Math.max(0, currentPage - averageOfTotalButtons);
+  const endPage = Math.min(totalPages, startPage + totalAmountOfButtons);
+  if (currentPage + averageOfTotalButtons >= totalPages) {
+    startPage = Math.max(0, totalPages - totalAmountOfButtons);
+  }
+  return [...new Array(endPage - startPage)].map((_, index) => (
     <li className="page-item" key={index}>
       <Link
-        className={`page-link ${index === currentPage ? "disabled" : ""}`}
-        href={`?page=${index}`}
+        className={`page-link ${
+          index + startPage === currentPage ? "disabled" : ""
+        }`}
+        href={`?page=${startPage + index}`}
       >
-        {index + 1}
+        {startPage + index + 1}
       </Link>
     </li>
   ));
@@ -67,7 +85,6 @@ export const NextPageButton = ({ currentPage, totalPages }) => {
     </li>
   );
 };
-
 export const LastPageButton = ({ currentPage, totalPages }) => {
   return (
     <li className="page-item">
@@ -82,7 +99,6 @@ export const LastPageButton = ({ currentPage, totalPages }) => {
     </li>
   );
 };
-
 export const Pagination = ({ totalPages, currentPage }) => {
   return (
     <nav aria-label="Navigation" className="d-flex justify-content-center ">
@@ -96,9 +112,7 @@ export const Pagination = ({ totalPages, currentPage }) => {
     </nav>
   );
 };
-
 const selectionModeTypes = ["single", "multiple", "none"];
-
 const DataTable = (props) => {
   const {
     name,
@@ -106,104 +120,113 @@ const DataTable = (props) => {
     dataSource,
     dataKey,
     children,
+    error,
     selectionMode = "none", //single, multiple, none
     totalPages = 0,
     currentPage = 0,
     pageSize = 0,
   } = props;
-
   const [selectedItems, setSelectedItems] = useState([]);
-
   if (!name) throw new Error("Missing name for datatable");
-
   if (!dataSource || !Array.isArray(dataSource))
     throw new Error("dataSource attribute is required for datatable");
-
   if (!dataKey) throw new Error("dataKey attribute is required for datatable");
-
   if (!children) throw new Error("Column required for datatable");
-
   const columns = Array.isArray(children) ? [...children] : [children];
-
   if (!selectionModeTypes.includes(selectionMode))
     throw new Error("Invalid selection mode for datatable");
-
   if (selectionMode !== "none") {
-    columns.splice(0, 0, <Column selectionMode={selectionMode}></Column>);
+    // Alltaki key="" uniqe key hatasini ortadan kaldirmak icin kullanildi
+    columns.splice(
+      0,
+      0,
+      <Column selectionMode={selectionMode} key=""></Column>
+    );
   }
 
-  const handleSelectedItems = (e) => {
-    const val = e.target.value;
+  const handleSelectedItems = (val) => {
     let arr = [...selectedItems];
-
     if (!arr.includes(val)) {
       arr.push(val);
     } else {
       arr = arr.filter((item) => item !== val);
     }
-
     setSelectedItems(arr);
   };
 
-  console.log(selectedItems);
-
   return (
-    <div className="card">
-      <input type="hidden" name={name} value={JSON.stringify(selectedItems)} />
-      <div className="card-body">
-        <h3 className="card-title">{title}</h3>
-        <div className="table-responsive">
-          <table className="table table-striped">
-            <thead>
-              <tr>{columns}</tr>
-            </thead>
-            <tbody>
-              {dataSource.map((row, rowIndex) => (
-                <Row key={row[dataKey]} selectionMode={selectionMode}>
-                  {columns.map((cell) => {
-                    const { dataField, index, template, selectionMode } =
-                      cell.props;
+    <>
+      <div className={`card ${error ? "border-danger" : ""}`}>
+        <input
+          type="hidden"
+          name={name}
+          value={JSON.stringify(selectedItems)}
+        />
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <div className="table-responsive">
+            <table className="table table-striped table-hover ">
+              <thead>
+                <tr>{columns}</tr>
+              </thead>
+              <tbody>
+                {dataSource.map((row, rowIndex) => {
+                  const id = row[dataKey];
+                  const selected = selectedItems.includes(id);
 
-                    let cellData = "";
-                    const cellKey = row[dataKey] + dataField + cellData;
-
-                    if (index) {
-                      cellData = pageSize * currentPage + rowIndex + 1;
-                    } else if (dataField) {
-                      cellData = row[dataField];
-                    } else if (template) {
-                      cellData = template(row);
-                    } else if (selectionMode !== "none") {
-                      const inputType =
-                        selectionMode === "single" ? "radio" : "checkbox";
-                      cellData = (
-                        <input
-                          type={inputType}
-                          name="rd"
-                          className="form-check-input"
-                          defaultValue={row[dataKey]}
-                          onChange={handleSelectedItems}
-                        />
-                      );
-                    }
-
-                    return <Cell key={cellKey}>{cellData}</Cell>;
-                  })}
-                </Row>
-              ))}
-            </tbody>
-          </table>
-
-          {totalPages > 1 ? (
-            <Pagination totalPages={totalPages} currentPage={currentPage} />
-          ) : null}
+                  return (
+                    <Row
+                      key={id}
+                      selectionMode={selectionMode}
+                      selected={selected}
+                      onClick={() => handleSelectedItems(id)}
+                    >
+                      {columns.map((cell, colIndex) => {
+                        const { dataField, index, template, selectionMode } =
+                          cell.props;
+                        let cellData = "";
+                        const cellKey = `${id}-${colIndex}`;
+                        if (index) {
+                          cellData = pageSize * currentPage + rowIndex + 1;
+                        } else if (dataField) {
+                          cellData = row[dataField];
+                        } else if (template) {
+                          cellData = template(row);
+                        } else if (selectionMode !== "none") {
+                          const inputType =
+                            selectionMode === "single" ? "radio" : "checkbox";
+                          cellData = (
+                            <input
+                              type={inputType}
+                              name="rd"
+                              className="form-check-input"
+                              defaultValue={id}
+                              onChange={(e) =>
+                                handleSelectedItems(e.target.value)
+                              }
+                              checked={selected}
+                            />
+                          );
+                        }
+                        return <Cell key={cellKey}>{cellData}</Cell>;
+                      })}
+                    </Row>
+                  );
+                })}
+              </tbody>
+            </table>
+            {totalPages > 1 ? (
+              <Pagination totalPages={totalPages} currentPage={currentPage} />
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
+      {error ? <div className="text-danger">{error}</div> : null}
+    </>
   );
 };
-
 export default DataTable;
+
 /*
   if(index){
     cellData = rowIndex+1;
